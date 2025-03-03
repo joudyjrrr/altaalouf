9ccecfc1c3107eeb8b6ea506af496b10174eb3f4
Joudy Jreis <Front-End Developer@Altaalouf.local>
2025-03-03 10:27:10 +0300

    add some page

--- public/images/index.jsx
+++ public/images/index.jsx
@@ -101,11 +101,6 @@ import flag from "./flag.png";
 import freq_aps from "./freq_aps.png";
 import plus from "./plus.png";
 
-import about1 from "./about1.png";
-import about2 from "./about2.png";
-import about3 from "./about3.png";
-import about4 from "./about4.png";
-import about5 from "./about5.png";
 
 export {
   BNB,
@@ -117,7 +112,6 @@ export {
   real_account,
   about2,
   tutorial,
-  about2,
   about3,
   about4,
   about5,
